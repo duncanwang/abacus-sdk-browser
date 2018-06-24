ceea33ae908e5b03c939cef7c08ce5e2024df5aa
Benny Yan <bnynn.gml@gmail.com>
2018-06-24 11:27:57 -0700

    working examples

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import sso from '@abacusprotocol/sso';
+import abacus from '@abacusprotocol/client-sdk';
 
 class App extends Component {
   componentDidMount() {
-    console.log(sso);
-    sso.init({
+    console.log(abacus);
+    abacus.init({
       portalURL: "https://identity.abacusprotocol.com",
       apiURL: "https://backend.abacusprotocol.com",
       application: "e8ea696f-20ed-43b3-84ac-68d372959e6a"
@@ -16,7 +16,7 @@ class App extends Component {
         <button
           type="primary"
           onClick={() => {
-            sso.openModal({
+            abacus.openModal({
               onClose() {
                 console.log("test123");
               }
@@ -28,7 +28,7 @@ class App extends Component {
         <button
           type="primary"
           onClick={async () => {
-            const result = await sso.fetchVerificationStatus(
+            const result = await abacus.fetchVerificationStatus(
               // @ts-ignore
               window.web3.eth.defaultAccount
             );
