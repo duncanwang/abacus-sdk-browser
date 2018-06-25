ef2b27f1a8a02d226d079c09bdd0da4890da301d
Benny Yan <bnynn.gml@gmail.com>
2018-06-25 02:08:13 -0700

    basic title added to house app

--- examples/house-dapp/src/App.js
+++ examples/house-dapp/src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./App.css";
-import logo from "./logo.svg";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="header">
+          <h1 className="title">House Marketplace DApp</h1>
+        </div>
       </div>
     );
   }
