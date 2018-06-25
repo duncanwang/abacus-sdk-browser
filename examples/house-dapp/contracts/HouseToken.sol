09d5803ff71c8266071188210a2a4835308891f6
Ian Macalinao <me@ian.pw>
2018-06-25 14:58:11 -0700

    allow specifying token uri in contract

--- examples/house-dapp/contracts/HouseToken.sol
+++ examples/house-dapp/contracts/HouseToken.sol
@@ -4,8 +4,10 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 contract HouseToken is ERC721Token("HouseToken", "AHT") {
 
-  function create() public {
-    _mint(msg.sender, allTokens.length + 1);
+  function create(string uri) public {
+    uint256 id = allTokens.length + 1;
+    _mint(msg.sender, id);
+    _setTokenURI(id, uri);
   }
 
 }
