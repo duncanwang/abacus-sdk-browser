fef198f39ea1461d5890486815908ebecf81da5c
Benny Yan <bnynn.gml@gmail.com>
2018-06-25 02:06:40 -0700

    fix config and basic;

--- examples/house-dapp/src/App.js
+++ examples/house-dapp/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
