67b9c6586039df38f83fa5711ecd7666b56be6fb
Pradyuman Vig <pradyumanvig@outlook.com>
2018-06-25 17:07:18 -0700

    HouseToken::mintTo

--- examples/house-dapp/contracts/HouseToken.sol
+++ examples/house-dapp/contracts/HouseToken.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract HouseToken is ERC721Token("HouseToken", "AHT") {
+contract HouseToken is ERC721Token("HouseToken", "AHT"), Ownable {
 
-  function create(string uri) public {
-    uint256 id = allTokens.length + 1;
-    _mint(msg.sender, id);
-    _setTokenURI(id, uri);
+  /**
+  * @dev Mints a token to an address with a tokenURI.
+  * @param _to address of the future owner of the token
+  * @param _tokenURI token URI for the token
+  */
+  function mintTo(address _to, string _tokenURI) public onlyOwner {
+      uint256 newTokenId = totalSupply().add(1);
+      _mint(_to, newTokenId);
+      _setTokenURI(newTokenId, _tokenURI);
   }
 
 }
