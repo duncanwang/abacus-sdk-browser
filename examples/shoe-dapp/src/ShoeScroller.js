64a8e9b51683da2ab0112915bf91fe4df7006828
Benny Yan <bnynn.gml@gmail.com>
2018-06-27 01:36:07 -0700

    created slider

--- examples/shoe-dapp/src/ShoeScroller.js
+++ examples/shoe-dapp/src/ShoeScroller.js
@@ -2,9 +2,28 @@ import React from "react";
 import "./App.css";
 
 class ShoeScroller extends React.Component {
+  state = {
+    pos: 36
+  };
   render() {
     const { url } = this.props;
-    return <div className="shoe-scroller">{url}</div>;
+    return (
+      <div className="shoe-scroller">
+        <img src={url + "img" + this.state.pos + ".jpg"} />
+        <input
+          type="range"
+          min="1"
+          max="36"
+          onChange={e =>
+            this.setState({
+              pos: e.target.value < 10 ? "0" + e.target.value : e.target.value
+            })
+          }
+          value={this.state.pos}
+          class="slider"
+        />
+      </div>
+    );
   }
 }
 
