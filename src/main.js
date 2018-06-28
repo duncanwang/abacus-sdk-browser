4480e286808efe7c15bc12ede0661a99aa73a057
Ian Macalinao <me@ian.pw>
2018-06-27 22:46:22 -0700

    More docus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "@babel/polyfill";
 import fetch from "isomorphic-unfetch";
+import * as qs from "qs";
 
 var ERRORS = {
   AUTH: "AuthenticationError"
@@ -11,15 +12,31 @@ function AbacusError(message, name) {
   return instance;
 }
 
+const parseJWT = token => {
+  if (typeof token !== "string") return null;
+  return JSON.parse(atob(token.split(".")[1]));
+};
+
+/**
+ * Abacus SDK main class.
+ */
 class Abacus {
+  /**
+   * Instantiates the Abacus SDK.
+   *
+   * @param {Object} params
+   * @param {Object} params.authToken An auth token to use the API with, if it exists.
+   * @param {Object} params.applicationId The ID of your application. You can obtain this at https://identity.abacusprotocol.com/application/admin/create_application.
+   * @param {Object} params.portalURL The URL to the Abacus Portal dApp.
+   * @param {Object} params.apiURL The URL to the Abacus API.
+   */
   constructor(params) {
     if (!params.applicationId)
-      throw new AbacusError("Please provide an application ID");
+      throw new AbacusError("Please provide an application ID.");
     this._opts = {
       portalURL: params.portalURL || "https://identity.abacusprotocol.com",
       apiURL: params.apiURL || "https://api.abacusprotocol.com",
-      applicationId: params.applicationId,
-      requireKYC: !!params.requireKYC || false
+      applicationId: params.applicationId
     };
     this.MODAL_ID = "abacusSDK";
     this._displaying = false;
@@ -31,8 +48,14 @@ class Abacus {
     }
   }
 
-  closeModal(modal, onClose) {
+  /**
+   * Closes the modal if it's open.
+   *
+   * @param {function} onClose Called if and when the modal is closed.
+   */
+  closeModal(onClose) {
     if (!this._displaying) return;
+    const modal = document.getElementById(this.MODAL_ID);
     modal.style.display = "none";
     this._displaying = false;
     onClose();
@@ -40,8 +63,16 @@ class Abacus {
 
   /* AUTHENTICATION METHODS */
 
+  /**
+   * Opens the Abacus modal for login and (optionally) user verification.
+   *
+   * @param {Object} options
+   * @param {function} onOpen Called when the modal is opened.
+   * @param {function} onClose Called when the modal is closed.
+   * @param {Boolean} runVerifications True if the modal should include the verifications flow for the application.
+   */
   authorizeWithModal(options) {
-    var OPTS = {
+    const OPTS = {
       onOpen:
         options && typeof options.onOpen === "function"
           ? options.onOpen
@@ -49,19 +80,22 @@ class Abacus {
       onClose:
         options && typeof options.onClose === "function"
           ? options.onClose
-          : function() {}
+          : function() {},
+      runVerifications: !!options.runVerifications || false
     };
+
+    const query = {};
+    if (this._opts.applicationId) {
+      query.application = this._opts.applicationId;
+    }
+    if (this._opts.runVerifications) {
+      query.requireKYC = "true";
+    }
 
     var modal = document.getElementById(this.MODAL_ID);
     if (!modal) {
       modal = document.createElement("iframe");
-      modal.src =
-        (this._opts.portalURL || "http://localhost:3000") +
-        "/modal/login?" +
-        (this._opts.applicationId
-          ? "application=" + this._opts.applicationId + "&"
-          : "") +
-        (this._opts.requireKYC ? "requireKYC=true&" : "");
+      modal.src = this._opts.portalURL + "/modal/login?" + qs.stringify(query);
       modal.width = "100%";
       modal.height = "100%";
       modal.frameBorder = "0";
@@ -85,11 +119,11 @@ class Abacus {
       window.addEventListener("message", function(event) {
         // TODO: deprecated this part
         if (event.data === "abacus_modal_close") {
-          this.closeModal(modal, OPTS.onClose);
+          this.closeModal(OPTS.onClose);
         }
         if (event.data.name !== "abacus") return;
         if (event.data.payload === "modal_close") {
-          this.closeModal(modal, OPTS.onClose);
+          this.closeModal(OPTS.onClose);
         }
         if (event.data.payload.appToken) {
           this._authUser = event.data.payload.appToken;
@@ -128,11 +162,6 @@ class Abacus {
     });
   }
 
-  parseToken(token) {
-    if (typeof token !== "string") return null;
-    return JSON.parse(atob(token.split(".")[1]));
-  }
-
   readAuthToken() {
     return this.parseToken(this._authUser);
   }
@@ -144,6 +173,10 @@ class Abacus {
 
   /* USER METHODS */
 
+  /**
+   * Fetches a list of all identity verifications performed on the user.
+   * @returns <Object> A map of verification type to status.
+   */
   async fetchVerifications() {
     const user = this.readAuthToken();
     return await this._sendGetRequest(
@@ -156,6 +189,15 @@ class Abacus {
 
   /* ANNOTATION METHODS */
 
+  /**
+   * Writes annotations for the current user.
+   *
+   * @param {Object} data
+   * @param {Object} data.ethereum
+   * @param {Object} data.ethereum.bytes32 Key-value mapping of bytes32 data to store on-chain. The key can be any string, and the value must be a hex-encoded string.
+   * @param {Object} data.ethereum.bytes Key-value mapping of bytes data to store on-chain. The key can be any string, and the value must be a hex-encoded string.
+   * @param {Object} data.private Key-value mapping of data to store off-chain.
+   */
   async writeUserAnnotations(data) {
     const user = this.readAuthToken();
     return await this._sendPostRequest(
@@ -166,6 +208,9 @@ class Abacus {
     );
   }
 
+  /**
+   * Fetches a list of all annotations on the user.
+   */
   async fetchUserAnnotations() {
     const user = this.readAuthToken();
     return await this._sendGetRequest(
@@ -176,6 +221,17 @@ class Abacus {
     );
   }
 
+  /**
+   * Writes annotations for a specific token.
+   *
+   * @param {Object} address The address of the token.
+   * @param {Object} tokenId The id of the token.
+   * @param {Object} data
+   * @param {Object} data.ethereum
+   * @param {Object} data.ethereum.bytes32 Key-value mapping of bytes32 data to store on-chain. The key can be any string, and the value must be a hex-encoded string.
+   * @param {Object} data.ethereum.bytes Key-value mapping of bytes data to store on-chain. The key can be any string, and the value must be a hex-encoded string.
+   * @param {Object} data.private Key-value mapping of data to store off-chain.
+   */
   async writeTokenAnnotations({ address, tokenId, data }) {
     return await this._sendPostRequest(
       `/applications/${
@@ -185,6 +241,12 @@ class Abacus {
     );
   }
 
+  /**
+   * Fetches a list of all annotations on a specific token.
+   *
+   * @param {Object} address The address of the token.
+   * @param {Object} tokenId The id of the token.
+   */
   async fetchTokenAnnotations({ address, tokenId }) {
     return await this._sendGetRequest(
       `/applications/${
